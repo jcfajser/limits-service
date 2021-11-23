4ad357229f0b8af6160048469e6eca4c42e48ad4
Juan Carlos Fajardo <juan.fajardo@softtek.com>
2021-11-23 14:42:53 -0600

    Initial commit

---
+++ src/main/java/com/fajardo/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,19 @@
+package com.fajardo.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public LimitConfiguration retriveLimitsFromConfigurations(){
+        //return new LimitConfiguration(1000, 1);
+         // getting values from the properties file
+        return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+    }
+}
